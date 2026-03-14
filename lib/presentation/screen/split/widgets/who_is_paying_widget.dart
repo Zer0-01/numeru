763cbb0931e9ec37b3b0d6cb649a4c2ef08c7b8a
Anas <anas.zulkifli2001@gmail.com>
2026-03-14 08:21:00 +0800

    feat: Add a confirmation dialog when removing a person from the split.

--- lib/presentation/screen/split/widgets/who_is_paying_widget.dart
+++ lib/presentation/screen/split/widgets/who_is_paying_widget.dart
@@ -4,6 +4,9 @@ import 'package:numeru/constant/padding_constant.dart';
 import 'package:numeru/constant/sizes_constant.dart';
 import 'package:numeru/data/models/person_model.dart';
 import 'package:numeru/extensions/context_extension.dart';
+import 'package:numeru/presentation/common_widgets/app_dialog_widget.dart';
+import 'package:numeru/presentation/common_widgets/buttons/app_filled_button_widget.dart';
+import 'package:numeru/presentation/common_widgets/buttons/app_outlined_button_widget.dart';
 import 'package:numeru/presentation/screen/split/bloc/split_bloc.dart';
 import 'package:numeru/util/common_functions.dart';
 
@@ -192,8 +195,27 @@ class _WhoIsPayingWidgetState extends State<WhoIsPayingWidget> {
                     top: -2,
                     child: GestureDetector(
                       onTap: () {
-                        context.read<SplitBloc>().add(
-                          OnRemovePeopleEvent(person.id),
+                        showDialog(
+                          context: context,
+                          builder:
+                              (dialogContext) => AppDialogWidget.alert(
+                                title: "Remove Person",
+                                subtitle:
+                                    "Are you sure you want to remove ${person.name}?",
+                                secondaryButton: AppOutlinedButtonWidget(
+                                  label: "Cancel",
+                                  onPressed: () => Navigator.pop(dialogContext),
+                                ),
+                                primaryButton: AppFilledButtonWidget(
+                                  label: "Remove",
+                                  onPressed: () {
+                                    context.read<SplitBloc>().add(
+                                      OnRemovePeopleEvent(person.id),
+                                    );
+                                    Navigator.pop(dialogContext);
+                                  },
+                                ),
+                              ),
                         );
                       },
                       child: Container(
